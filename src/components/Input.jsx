18f91bdf0ff3f381e627c2d78de8fae448712b80
Mark Linn <marktlinn@gmail.com>
2022-07-30 20:03:51 +0200

    switched the CSS to SCSS, started styling, finished nav

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 export default function Input(){
     return (
         <section className='input-container'>
-            <form action="">
+            <form  action="">
                 <div className='input-fields'>
                     <input className='text-box' type="text" />
                     <input className='text-box' type="text" />
                 </div>
-                <button type='submit' className='generate-meme-btn'></button>
+                <div className='btn-section'>
+                <button type='submit' className='generate-meme-btn'>Get A New Image</button>
+                </div>
             </form>
         </section>
     )
