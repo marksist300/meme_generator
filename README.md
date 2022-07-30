14c20e225295a0b2b144b2cbdb35ee56629e81b6
Mark Linn <marktlinn@gmail.com>
2022-07-30 17:32:46 +0200

    first commit, file set up

---
+++ README.md
@@ -0,0 +1,15 @@
+# A Meme Generator
+
+
+**Link to project:** 
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, React, Figma
+
+
+## Optimizations
+
+
+## Lessons Learned:
+
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <p>WORLD</p>
+    </div>
+  )
+}
+
+export default App
