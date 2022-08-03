b8d6d601d7980e1589943fbb5bbde7def3d91d20
Mark Linn <marktlinn@gmail.com>
2022-08-03 22:23:44 +0200

    added a light and dark theme toggle switch, without functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,18 @@ export default function Nav () {
     return (
         <header>
             <nav className='nav-bar'>
+                <div className='title-nav'>
                 <img src={trollFace} alt="Distorted face of a troll, twisted to the right, a simple drawing" />
                 <h1>Meme Generator</h1>
+                </div>
+
+                <div className="toggler">
+                    <p className="toggler-light">Light</p>
+                    <div className="toggler-slider">
+                        <div className="toggler-slider-circle"></div>
+                    </div>
+                    <p className="toggler-dark">Dark</p>
+                </div>
             </nav>
         </header>
     )
